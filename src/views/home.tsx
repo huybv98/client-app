4e1efdc934f74a4c368e15a7d3ca2a2f83232684
Huybv98 <mcnb.1998@gmail.com>
2023-03-08 11:27:17 +0700

    short import link

---
+++ src/views/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from 'components/layout'
+const Home = () => {
+  return (
+    <>
+      <header className="App-header">
+        <Layout />
+      </header>
+      <div>HelloWorld</div>
+    </>
+  )
+}
+
+export default Home
